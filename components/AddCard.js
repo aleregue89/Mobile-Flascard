679a508715488aa330505fd3832e02cb43e2df34
aleregue89 <aleregue89@gmail.com>
2020-10-11 19:56:46 -0400

    test

--- components/AddCard.js
+++ components/AddCard.js
@@ -25,6 +25,7 @@ export class AddCard extends Component {
 
     // handlesubmit
     handleSubmit = () => {
+        
         const {navigation} = this.props
         const {route} = this.props
         const {item} = route.params
